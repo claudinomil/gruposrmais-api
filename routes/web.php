a947032bb80058c7434e3d6d5e69b06077763d34
claudinomil <claudinomoraes@yahoo.com.br>
2024-10-22 18:35:13 -0300

    Reiniciando Projeto

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,31 @@ Route::post('/criarsubmodulos', [CriarSubmodulos::class, 'store'])->name('criars
 Route::get('empresas_grupo_srmais', function () {
     return \App\Models\Empresa::all();
 });
+
+//Limpar Caches via Navegador - Início''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Route::get('/clear-all-cache', function() {
+    $retorno = '';
+
+    //Limpar cache do aplicativo:
+    Artisan::call('cache:clear');
+    $retorno .= 'cache:clear'.'<br>';
+
+    //Limpar cache de rota:
+    Artisan::call('route:cache');
+    $retorno .= 'route:cache'.'<br>';
+
+    //Limpar cache de configuração:
+    Artisan::call('config:cache');
+    $retorno .= 'config:cache'.'<br>';
+
+    //Clear view cache:
+    Artisan::call('view:clear');
+    $retorno .= 'view:clear'.'<br>';
+
+    //Limpe todo o aplicativo de todos os tipos de cache:
+    Artisan::call('optimize:clear');
+    $retorno .= 'optimize:clear'.'<br>';
+
+    echo $retorno;
+});
+//Limpar Caches via Navegador - Fim'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
